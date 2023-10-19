e0c5baeb4d1205a8b90d65d136a3de77c9c57825
Davide Ponzini <davide.ponzini95@gmail.com>
2023-10-19 15:52:01 +0200

    updated footer

--- components/footer.php
+++ components/footer.php
@@ -1,12 +1,15 @@
 <div class="footer">
     <div class="container">
-        <div class="center" style="font-family: monospace">
+        <div class="center monospace">
             Website developed by Davide Ponzini.
         </div>
 
         <!-- Social -->
         <div class="center">
             <a href="https://github.com/DavidePonzini" class="bi bi-github social social-github"></a>
+        </div>
+        <div class="right monospace">
+            Your IP Address: <?php echo $_SERVER['REMOTE_ADDR']; ?>
         </div>
 
         <!-- Privacy Policy -->
