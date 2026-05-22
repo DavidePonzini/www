7a5cd551ed5e3744e92d1fe07000969292ce6898
Davide Ponzini <davide.ponzini95@gmail.com>
2026-05-22 13:45:35 +0200

    renamed component

--- webui/src/components/flow/index.ts
+++ webui/src/components/flow/index.ts
@@ -2,4 +2,4 @@ export { default as Flow } from './Flow';
 export { default as Sequence } from './Sequence';
 export { default as Step } from './Step';
 export { default as Parallel } from './Parallel';
-export { default as Branch } from './Branch';
+export { default as ParallelBranch } from './Branch';
